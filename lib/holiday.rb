31dc79a38d1e3020844119ef730954f2056aca50
synchronous-environment-3669 <kyu22@buffalo.edu>
2018-11-25 22:58:38 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -52,7 +52,7 @@ end
 
 def all_winter_holiday_supplies(holiday_hash)
   # return an array of all of the supplies that are used in the winter season
-  holiday_hash_hash[:winter].values.each do |x|
+  holiday_hash[:winter].values.each do |x|
     puts "#{x}"
 end
 
