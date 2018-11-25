953e42ebf168e71ab7321b06edeaeeb24bd4caa9
synchronous-environment-3669 <kyu22@buffalo.edu>
2018-11-25 23:05:04 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -80,7 +80,7 @@ def all_holidays_with_bbq(holiday_hash)
     holiday.each do |holiday, supplies_array|
       supplies_array.each do |supply|
         if supply == "BBQ"
-         answer.push(holiday_name)
+         answer.push(holiday)
        end
      end
   end
